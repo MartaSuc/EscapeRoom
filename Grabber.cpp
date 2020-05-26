72b7a48f42053040642e60639758398e77fb9c79
MartaSuc <62238657+MartaSuc@users.noreply.github.com>
2020-05-26 15:05:18 +0200

    Update Grabber.cpp

--- Grabber.cpp
+++ Grabber.cpp
@@ -1,9 +1,5 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Grabber.h"
 #include "GameFramework/PlayerController.h"
-#include "DrawDebugHelpers.h"
 #include "Engine/World.h"
 
 #define OUT
@@ -23,45 +19,58 @@ void UGrabber::BeginPlay()
 	FindPhysicsHandle();
 	SetUpInputComponent();
 }
+
 void UGrabber::SetUpInputComponent()
 {
 	InputCompon = GetOwner()->FindComponentByClass<UInputComponent>();
-	if (InputCompon) 
+	if (InputCompon)
 	{
 		InputCompon->BindAction("Grab", EInputEvent::IE_Pressed, this, &UGrabber::Grab);
 		InputCompon->BindAction("Grab", EInputEvent::IE_Released, this, &UGrabber::Release);
 	}
 }
+
 void UGrabber::FindPhysicsHandle()
 {
 	HandlePhysics = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
-	if (HandlePhysics == nullptr)  
+	if (HandlePhysics == nullptr)
 		UE_LOG(LogTemp, Error, TEXT("No PhysicsHandle component found on: %s!"), *GetOwner()->GetName());
 }
+
 void UGrabber::GetLocations()
 {
 	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT PlayerViewPointLocation, OUT PlayerViewPointRotation);
-	LineTraceEnd = PlayerViewPointLocation + PlayerReach * PlayerViewPointRotation.Vector();
+	LineTraceEnd = PlayerViewPointLocation + PlayerReach *PlayerViewPointRotation.Vector();
 }
+
 void UGrabber::Grab()
 {
 	GetLocations();
 	FHitResult HitResult = GetFirstPhysicsBodyInReach();
-	UPrimitiveComponent* ComponentToGrab = HitResult.GetComponent();
-	UE_LOG(LogTemp, Warning, TEXT("Grab pressed")); //debug line
-	
 	if (HitResult.GetActor())
-		HandlePhysics->GrabComponentAtLocation(
+	{
+		HitYaw = HitResult.GetActor()->GetActorRotation().Yaw;
+		TargetRotation = { 0.f, HitYaw, 0.f };
+		HitResult.GetActor()->SetActorRotation(TargetRotation);			//we want to pick objects that are set to straight position
+	UE_LOG(LogTemp, Warning, TEXT("Grab pressed"));			//debug line
+
+	UPrimitiveComponent* ComponentToGrab = HitResult.GetComponent();
+
+		HandlePhysics->GrabComponentAtLocationWithRotation( 
 			ComponentToGrab,
 			NAME_None,
-			LineTraceEnd);
+			LineTraceEnd,
+			TargetRotation);
+	}
 }
+
 void UGrabber::Release()
 {
-	UE_LOG(LogTemp, Warning, TEXT("Grab released")); //debug line
+	UE_LOG(LogTemp, Warning, TEXT("Grab released"));		 //debug line
 	if (HandlePhysics->GrabbedComponent)
 		HandlePhysics->ReleaseComponent();
 }
+
 FHitResult UGrabber::GetFirstPhysicsBodyInReach() const
 {
 	FHitResult Hit;
@@ -73,18 +82,21 @@ FHitResult UGrabber::GetFirstPhysicsBodyInReach() const
 		LineTraceEnd,
 		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
 		TraceParams);
-//debug log
+
+	//debug log
 	if (Hit.GetActor())
 		UE_LOG(LogTemp, Warning, TEXT("Actor hit: %s"), *Hit.GetActor()->GetName());
 
 	return Hit;
 }
+
 // Called every frame
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 	GetLocations();
 	if (HandlePhysics->GrabbedComponent)
-		HandlePhysics->SetTargetLocation(LineTraceEnd);
-
+	{
+		HandlePhysics->SetTargetLocationAndRotation(LineTraceEnd, TargetRotation);
+	}
 }
