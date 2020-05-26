373d7a48d9661b944f0d79af446fb65deabf5df5
MartaSuc <62238657+MartaSuc@users.noreply.github.com>
2020-05-26 15:10:50 +0200

    Create Exit.h

---
+++ Exit.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
+#include "Exit.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class ESCAPEROOM_API UExit : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UExit();
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	void FindTrigger();
+	float MassOfActorsOnTrigger(ATriggerVolume* Trigger);
+	void OpeningDoor(float DeltaTime);
+	void ClosingDoor(float DeltaTime);
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+private:
+	FVector CurrentLocation = { 0.f, 0.f, 0.f };
+	FVector OpenLocation = { 0.f, 0.f, 0.f };
+	FVector CloseLocation = { 0.f, 0.f, 0.f };
+	FVector Door = { 0.f, 0.f, 0.f };
+
+	UPROPERTY(EditAnywhere)
+		ATriggerVolume* ExitDoorTrig = nullptr;
+};
